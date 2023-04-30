234cc4a7477703bda4415b6927247cecd524c2af
gbrlcoelho <coelhogabriel67@gmail.com>
2023-04-30 13:16:48 -0300

    refactor(my_button.dart): refactor my button widget to use an elevated button instead of a container

--- lib/widgets/my_button.dart
+++ lib/widgets/my_button.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  final Function()? onTap;
-  const MyButton({Key? key, this.onTap}) : super(key: key);
+  final VoidCallback? onPressed;
+  final double width;
+  final bool isDisabled;
+
+  const MyButton({
+    Key? key,
+    this.onPressed,
+    required this.width,
+    this.isDisabled = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-            color: Colors.black, borderRadius: BorderRadius.circular(8)),
-        child: const Center(
-          child: Text(
-            'Sign In',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        onPressed: isDisabled ? null : onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          elevation: 4,
+          shadowColor: Colors.black.withOpacity(0.4),
+        ),
+        child: const Text(
+          'Sign In',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
           ),
         ),
       ),
