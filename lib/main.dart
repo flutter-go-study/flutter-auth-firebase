72316ea676e68c610f2db2a66070a46ac08719f2
gbrlcoelho <coelhogabriel67@gmail.com>
2023-04-30 13:17:21 -0300

    feat(auth_page.dart): created auth page to check if user is logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_auth_firebase/pages/login_page.dart';
+import 'package:flutter_auth_firebase/pages/auth_page.dart';
 
 import 'firebase_options.dart';
 
@@ -17,9 +17,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: AuthPage(),
     );
   }
 }
